386a7b40d0a4fe97baca479b72f8627b5b74971f
elmouftakir <abderahim.mouftakir@gmail.com>
2024-08-26 19:37:46 +0100

    Refactor code to structure API into namespaces with Flask-RESTX

--- api/__init__.py
+++ api/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_restx import Api
 from .orders.views import order_namespace
 from .auth.views import auth_namespace
+from .config.config import config_dict
 
-def create_app():
+def create_app(config=config_dict['dev']):
     app=Flask(__name__)
+    
+    
+    app.config.from_object(config)
     
     api=Api(app)
     
     
-    api.add_namespace(order_namespace,path='/')
+    api.add_namespace(order_namespace)
     api.add_namespace(auth_namespace,path='/auth')
     
     return app
