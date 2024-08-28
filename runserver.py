2cf7a2ef916f833834d240b0f719e04f3a5294bb
Ayaa05 <aya18abbadi@gmail.com>
2024-08-28 23:14:06 +0100

    runserver

--- runserver.py
+++ runserver.py
@@ -1,8 +1,10 @@
 from api import create_app
 from api.config.config import config_dict
 
+# Create a Flask application instance using the production configuration
+app = create_app(config=config_dict['production'])
 
-app=create_app(config=config_dict['production'])
-
+# Run the Flask application if this script is executed directly
 if __name__ == "__main__":
     app.run()
+
