c4d28d6e77e46ae91ab7bbb61b83f3585988a9c9
Ayaa05 <aya18abbadi@gmail.com>
2024-08-28 22:52:34 +0100

    init__.py

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -5,63 +5,24 @@ from ..models.orders import Order
 from ..utils import db
 from flask_jwt_extended import create_access_token
 
-
 class OrderTestCase(unittest.TestCase):
+    # Setup method to initialize the test environment
     def setUp(self):
-        self.app=create_app(config=config_dict['test'])
-        self.appctx=self.app.app_context()
-
+        # Create the Flask app instance with the test configuration
+        self.app = create_app(config=config_dict['test'])
+        # Establish an application context for the test
+        self.appctx = self.app.app_context()
+        
+        # Push the app context to make it active
         self.appctx.push()
-
-        self.client=self.app.test_client()
-
+        
+        # Create a test client to simulate HTTP requests to the app
+        self.client = self.app.test_client()
+        
+        # Create all database tables before each test
         db.create_all()
 
-
+    # Teardown method to clean up after each test
     def tearDown(self):
+        # Drop all tables in the database to ensure a clean state for the next test
         db.drop_all()
-
-        self.app=None
-
-        self.appctx.pop()
-
-        self.client=None
-
-
-    def test_get_all_orders(self):
-
-        token=create_access_token(identity='testuser')
-
-        headers={
-            "Authorization":f"Bearer {token}"
-        }
-
-        response=self.client.get('/orders/orders/',headers=headers)
-
-        assert response.status_code == 200
-
-        assert response.json == []
-
-
-    def test_create_order(self):
-        data={
-            "size":"LARGE",
-            "quantity":3,
-            "flavour":"Test Flavour"
-        }
-
-        token=create_access_token(identity='testuser')
-
-        headers={
-            "Authorization":f"Bearer {token}"
-        }
-
-
-        response=self.client.post('/orders/orders/',json=data,headers=headers)
-
-
-        assert response.status_code == 201
-
-        orders= Order.query.all()
-
-        assert len(orders) == 1
